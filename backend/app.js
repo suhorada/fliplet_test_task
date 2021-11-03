39da464700e3ecf06abcb46b1e2c60899875755d
Semen Suhorada <semen.suhorada@gmail.com>
2021-11-03 15:09:07 +0300

    add cors

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 require('dotenv').config();
 const indexRouter = require('./routes');
 
 const app = express();
+app.use(cors());
+app.options('*', cors());
 
 app.use(logger('dev'));
 app.use(express.json());
