2f484d8fe55c4298d8c4c96b4d98d169ca0a019d
Semen Suhorada <semen.suhorada@gmail.com>
2021-11-03 16:31:09 +0300

    invalid feed validation

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,11 +1,18 @@
-const indexService = require('../services');
+const indexService = require("../services");
 
 async function getDataFromRSS(req, res) {
   try {
     const response = await indexService.rssToJSON(req.query.rss_url);
+    if (response.data.feed)
+      if (
+        !response.data.feed.title ||
+        !response.data.feed.image ||
+        !response.data.feed.description
+      )
+        res.status(500).send("Invalid feed");
     res.status(200).send(response.data);
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.status(500).send(err);
   }
 }
